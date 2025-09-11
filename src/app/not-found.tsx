332c53bd0534de4e129096440a2a83f913dd001f
suvajitcodewalnut <suvajit@codewalnut.com>
2025-09-11 15:56:40 +0530

    improvement(error-page): add error heading and desc

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+// Modules
+import { monotonFont } from '@/helpers/font';
+
+const Errorpage: React.FC = () => {
+  return (
+    <div className="mt-25">
+      <div>
+        <h1
+          className={`${monotonFont.className} text-7xl md:text-8xl lg:text-9xl overflow-hidden duration-200 transition-all`}
+        >
+          404
+        </h1>
+        <p className="text-lg sm:text-xl md:text-2xl text-gray-600 font-medium mt-3">
+          Unfortunately page is not available <br /> Stay tuned!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Errorpage;
