a4ee7e82fc38d032629c03fa505a4011ebadb603
suvajitcodewalnut <suvajit@codewalnut.com>
2025-09-11 13:36:10 +0530

    enhance(header-cosmetic): add hover styles in mobile screen for quick-links

--- src/components/Ui/Pagination/Pagination.tsx
+++ src/components/Ui/Pagination/Pagination.tsx
@@ -17,10 +17,19 @@ const Pagination: React.FC<PaginationProps> = ({ cards, prevRoute, nextRoute }) 
               <Link
                 key={idx}
                 href={card.route}
-                className="relative w-[200px] h-[120px] px-6 py-10 rounded-[17px] shadow-md border text-md font-bold bg-black text-gray-300 flex items-center justify-center text-center transform hover:scale-105 transition duration-300 ease-in-out
-                before:content-[''] before:absolute before:inset-0 before:rounded-lg before:bg-[radial-gradient(circle_at_left_bottom,rgba(255,255,255,0.9),transparent_70%)] before:opacity-0 hover:before:opacity-100 before:blur-3xl before:transition before:duration-700 before:delay-100 before:ease-in-out overflow-hidden"
+                className="relative w-[200px] h-[120px] px-6 py-10 rounded-[17px] shadow-md border text-md font-bold 
+                 bg-black text-gray-300 flex items-center justify-center text-center 
+                 transform transition-all duration-300 ease-in-out 
+                 hover:scale-105 hover:shadow-2xl hover:text-white
+
+                 before:content-[''] before:absolute before:inset-0 before:rounded-lg 
+                 before:bg-[radial-gradient(circle_at_left_bottom,rgba(255,255,255,0.9),transparent_70%)] 
+                 before:opacity-0 before:blur-3xl 
+                 before:transition-opacity before:duration-700 before:ease-in-out 
+                 hover:before:opacity-100 
+                 overflow-hidden"
               >
-                {card.label}
+                <span className="relative z-10 transition-colors duration-500">{card.label}</span>
               </Link>
             ))}
           </div>
@@ -78,7 +87,7 @@ const Pagination: React.FC<PaginationProps> = ({ cards, prevRoute, nextRoute }) 
               <Link
                 key={idx}
                 href={card.route}
-                className="px-4 py-2 rounded-xl shadow-md border text-sm font-semibold bg-white text-gray-700 hover:bg-gray-100 transition-colors"
+                className="px-4 py-2 rounded-xl shadow-md border text-sm font-semibold bg-black text-gray-300 hover:bg-gray-100  hover:border-1 hover:border-black hover:text-black hover:scale-105 duration-300 ease-in transition-all"
               >
                 {card.label}
               </Link>
