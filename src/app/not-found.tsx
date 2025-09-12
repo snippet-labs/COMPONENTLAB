587e3f7deab6c2349d5784ba64d26f74f22aa7ec
suvajitcodewalnut <suvajit@codewalnut.com>
2025-09-12 12:41:51 +0530

    fix(test-case): resolved failing test-cases

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 // Modules
-import { monotonFont } from '@/helpers/font';
+import { monoton } from '@/helpers/font';
 import Pagination from '@/components/Ui/Pagination/Pagination';
 
 const Errorpage: React.FC = () => {
@@ -7,7 +7,7 @@ const Errorpage: React.FC = () => {
     <div className="mt-25">
       <div>
         <h1
-          className={`${monotonFont.className} text-7xl md:text-8xl lg:text-9xl overflow-hidden duration-200 transition-all`}
+          className={`${monoton.className} text-7xl md:text-8xl lg:text-9xl overflow-hidden duration-200 transition-all`}
         >
           404
         </h1>
