0245a7c3754a24803d252eb77fd132843e1a280d
suvajitcodewalnut <suvajit@codewalnut.com>
2025-09-16 13:23:47 +0530

    page(side-panel-installation): add format check

--- src/constants/Installation/DocumentationLinkItems.ts
+++ src/constants/Installation/DocumentationLinkItems.ts
@@ -1,7 +1,7 @@
-// Modules 
-import { LinkCardsTypes } from "@/types";
+// Modules
+import { LinkCardsTypes } from '@/types';
 
 export const DOCUMENTATION_LINKS: LinkCardsTypes[] = [
-    { label: 'React Icons', route: 'https://www.npmjs.com/package/react-icons' },
-    { label: 'TailwindCSS', route: 'https://tailwindcss.com/docs/installation/using-vite' },
-]
+  { label: 'React Icons', route: 'https://www.npmjs.com/package/react-icons' },
+  { label: 'TailwindCSS', route: 'https://tailwindcss.com/docs/installation/using-vite' },
+];
