2bcd0d6359bb23db4b09c21b62c50b887f635884
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-10-12 12:17:38 +0530

    fix: failing test-cases; format check

--- src/app/buttons/page.tsx
+++ src/app/buttons/page.tsx
@@ -9,7 +9,7 @@ const AccessibleButtonStarterPage = () => {
   return (
     <div className="min-h-screen" data-testid="button-starter-page">
       <Starter
-        starterTitle="Buttons"
+        starterTitle="Accessible Buttons"
         starterDescription="A button is a fundamental interactive element in web development used to trigger actions — such as submitting forms, opening dialogs, or performing navigation. Buttons provide users with clear, actionable controls that drive interaction within a website or application.
         Ensuring that buttons are accessible is crucial, as they must be usable by everyone, including people who rely on screen readers, keyboard navigation, or assistive technologies. Proper accessibility involves using semantic HTML (<button>), clear labeling, focus states, and ARIA attributes where necessary. This not only improves usability but also enhances the overall inclusiveness and user experience of the application."
         starterVariantDescription="Starter variant description goes here"
