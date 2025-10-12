0535df7234a905ebefd63f803fcb8477863ce4cd
SUVAJIT-KARMAKAR <suvajit2001k@gmail.com>
2025-10-12 12:25:38 +0530

    page(accessible-component-dropdown-button): add proper description for button starter page

--- src/app/buttons/page.tsx
+++ src/app/buttons/page.tsx
@@ -12,7 +12,7 @@ const AccessibleButtonStarterPage = () => {
         starterTitle="Accessible Buttons"
         starterDescription="A button is a fundamental interactive element in web development used to trigger actions — such as submitting forms, opening dialogs, or performing navigation. Buttons provide users with clear, actionable controls that drive interaction within a website or application.
         Ensuring that buttons are accessible is crucial, as they must be usable by everyone, including people who rely on screen readers, keyboard navigation, or assistive technologies. Proper accessibility involves using semantic HTML (<button>), clear labeling, focus states, and ARIA attributes where necessary. This not only improves usability but also enhances the overall inclusiveness and user experience of the application."
-        starterVariantDescription="Starter variant description goes here"
+        starterVariantDescription="Here are all the button variants we have so far"
         starterVariantCards={BUTTON_VARIANTS}
         data-testid="starter-component"
       />
