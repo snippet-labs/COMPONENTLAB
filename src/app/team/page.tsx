ec3a1c1baba50dac1c2482452b4ee0776d9ae6a5
suvajitcodewalnut <suvajit@codewalnut.com>
2025-10-15 17:25:27 +0530

    page(team): add team page details

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,6 +1,7 @@
 // Modules
 import React from 'react';
 import { TEAM_PAGINATION_LINKS } from '@/constants/Team/TeamPaginationLinks';
+import ChatBubble from '@/components/Ui/Chat/ChatBubble';
 import Footer from '@/components/Ui/Footer/Footer';
 import Pagination from '@/components/Ui/Pagination/Pagination';
 
@@ -11,7 +12,246 @@ const TeamPage: React.FC = () => {
         <h1 className="ml-1 text-sm font-medium">COMPONENTLAB/</h1>
         <h2 className="font-bold text-4xl md:text-5xl lg:text-6xl">Read our team conversation</h2>
       </div>
-      <div className="mt-20"></div>
+      <div className="mt-20">
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="Hi, You guys free ?"
+          time="3:32 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemangshu"
+          message="Yup, what's up ?"
+          time="3:33 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Yess"
+          time="3:33 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="I was thinking of something fun, you know, let's create something!"
+          time="3:35 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Interesting ! I am in!"
+          time="3:36 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="What you have anything in mind ?"
+          time="3:37 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemangshu"
+          message="That's awesome!"
+          time="3:38 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="A component library ?"
+          time="3:39 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Sounds fascinating, our own library ??"
+          time="3:40 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="You in Hemangshu ?"
+          time="3:40 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Always Subhajit, although I was thinking what makes us different ? There are a lot of great component libraries out there !!!"
+          time="3:40 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="I'd be lying if I say this hadn't crossed my mind !"
+          time="3:42 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Something new ?"
+          time="3:43 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Ummmmmmm .... "
+          time="3:43 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Let's put the main focus in providing accessible components out of the box !"
+          time="3:44 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="What you think guys ?"
+          time="3:44 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="I believe that's a great idea man !"
+          time="3:45 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="Look if it works out, great ! If not, we will have a good project for our resume ! Isn't it ?"
+          time="3:45 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Absolutely !"
+          time="3:46 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Let's give this a go !"
+          time="3:47 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="What stack are we picking ??"
+          time="3:46 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="NextJs for great SEO, tailwindCSS for styling, Ummmm ... ViTest for unit testing and GitActions for complete CI/CD testing and build automation !"
+          time="3:47 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="Good ?"
+          time="3:49 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="It is going to be funnnnnn ! "
+          time="3:50 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Totally !"
+          time="3:50 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="We will have clean component design, just simple and elegant !"
+          time="3:52 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="Just copy and drop the code in the codebase kind of approach !"
+          time="3:52 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="We will mainly focus on providing accessible components with minimal styling"
+          time="3:53 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="Since we are using tailwindCSS, people can tweak it to whatever matches their taste !!"
+          time="3:54 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Correct !"
+          time="3:55 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="Definitely ! So .... ?"
+          time="3:56 PM"
+          isStart={true}
+        />
+        <ChatBubble
+          image={'/hemangshu.png'}
+          senderName="Hemanghsu"
+          message="Let's do this "
+          time="3:57 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/subhajit.png'}
+          senderName="Subhajit"
+          message="I am ready for this !"
+          time="3:58 PM"
+          isStart={false}
+        />
+        <ChatBubble
+          image={'/suvajit.png'}
+          senderName="Suvajit"
+          message="LET'S DO THIS GUYS !"
+          time="4:00 PM"
+          isStart={true}
+        />
+      </div>
       <Pagination
         paginationTitle="Explore our accessible components"
         cards={TEAM_PAGINATION_LINKS}
--- src/components/Ui/Chat/ChatBubble.tsx
+++ src/components/Ui/Chat/ChatBubble.tsx
@@ -12,23 +12,25 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({ image, senderName, message, tim
             <Image src={image} alt="Chat profile" width={100} height={100} />
           </div>
         </div>
-        <div className="chat-header">
+        <div className="chat-header text-sm md:text-md lg:text-lg">
           {senderName}
-          <time className="text-xs opacity-50">{time}</time>
+          <time className="text-xs font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 gradient-shift-animation">
+            {time}
+          </time>
         </div>
-        <div className="border-dotted border-2 bg-white shadow-2xl px-6 py-1 rounded-3xl">
+        <div className="text-sm md:text-xl lg:text-xl border-dotted border-2 bg-white shadow-2xl px-6 py-1 rounded-3xl font-bold">
           {message}
         </div>
         <div className="chat-footer opacity-50">
           {isStart ? (
             <>
-              <span className="font-bold">Delivered</span>
-              {<IoCheckmarkDoneSharp size={20} className="text-blue-700" />}
+              <span className="font-bold mt-2">Delivered</span>
+              {<IoCheckmarkDoneSharp size={20} className="mt-2 text-blue-700" />}
             </>
           ) : (
             <>
-              <span className="font-bold">Seen</span>
-              {<IoCheckmarkDoneSharp size={20} className="text-green-700" />}
+              <span className="font-bold mt-2">Seen</span>
+              {<IoCheckmarkDoneSharp size={20} className="mt-2 text-green-700" />}
             </>
           )}
         </div>
