3d52b9dd8d14ebb83ebf9ebed55e015dfb2e97ba
suvajitcodewalnut <suvajit@codewalnut.com>
2025-10-16 11:36:10 +0530

    chore: fix failing test-cases

--- src/app/team/page.test.tsx
+++ src/app/team/page.test.tsx
@@ -1,45 +1,47 @@
 // Modules
 import TeamPage from '@/app/team/page';
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 
 // Render
 const renderComponent = () => render(<TeamPage />);
 
-// Test suite
+// Test Suite
 describe('TeamPage Component', () => {
-  it('should render the header title correctly', () => {
+  beforeEach(() => {
     renderComponent();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should render header title correctly', () => {
     const headerTitle = screen.queryAllByTestId('team-header-title')[0];
     expect(headerTitle.textContent).toBe('COMPONENTLAB/');
   });
 
-  it('should render the subtitle correctly', () => {
-    renderComponent();
+  it('should render subtitle correctly', () => {
     const subtitle = screen.queryAllByTestId('team-subtitle')[0];
     expect(subtitle.textContent).toBe('Read our team conversation');
   });
 
-  it('should render the chat section', () => {
-    renderComponent();
+  it('should render chat section', () => {
     const chatSection = screen.queryAllByTestId('chat-section')[0];
     expect(chatSection).toBeTruthy();
   });
 
-  it('should render chat bubbles', () => {
-    renderComponent();
+  it('should render multiple ChatBubble components', () => {
     const chatBubbles = screen.queryAllByTestId('chatbubble');
     expect(chatBubbles).toBeTruthy();
   });
 
-  it('should render the pagination section', () => {
-    renderComponent();
+  it('should render pagination section', () => {
     const paginationSection = screen.queryAllByTestId('pagination-section')[0];
     expect(paginationSection).toBeTruthy();
   });
 
-  it('should render the footer section', () => {
-    renderComponent();
+  it('should render footer section', () => {
     const footerSection = screen.queryAllByTestId('footer-section')[0];
     expect(footerSection).toBeTruthy();
   });
